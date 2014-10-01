0376d3f1fcc32f316bde8a0df301c9d69d62b0ce
Brendan Sheehy <bsheehy@boomboxinc.com>
2014-10-01 10:46:17 -0400

    finished fifth

--- filter/program.js
+++ filter/program.js
@@ -6,11 +6,12 @@ var ext = process.argv[3];
 
 fs.readdir(dir, function(err, list){
 	for(i=0;i<list.length;i++){
-		console.log("list " + i + " is: " + list[i]);
-		console.log("Extension is " + path.extname(list[i]));
-		if(path.extname(list[i])==ext){
+		// console.log("list " + i + " is: " + list[i]);
+		// console.log("Extension is " + path.extname(list[i]));
+		if(path.extname(list[i]).substr(1)==ext){
 			console.log(list[i]);
 		}
 	}
 });
 
+// console.log("And ext is " + ext);
