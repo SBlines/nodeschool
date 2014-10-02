11e1b56a0ecf7f875a3102932ae7d5f48ef92bc3
Brendan Sheehy <bsheehy@boomboxinc.com>
2014-10-02 12:18:21 -0400

    completed eighth challenge

--- http_collect/program.js
+++ http_collect/program.js
@@ -4,15 +4,27 @@ var link = process.argv[2];
 
 http.get(link, function(response){
 	var combined = '';
-	var count = 0;
 	response.on("data", function(data){
 		combined += data;
-		count++;
-		// console.log(combined);
 	});
 	response.on("end", function(){
-		console.log(count);
+		console.log(combined.split("").length);
 		console.log(combined);
-
 	})
 });
+
+//nodeschool's answer (the second way):
+
+// var http = require('http')
+// var bl = require('bl')
+    
+// http.get(process.argv[2], function (response) {
+//   response.pipe(bl(function (err, data) {
+//     if (err)
+//       return console.error(err)
+//     data = data.toString()
+//     console.log(data.length)
+//     console.log(data)
+//   }))  
+// })
+
